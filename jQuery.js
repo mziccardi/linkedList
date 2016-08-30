255708e164e06ece8f4e10b23ea9edf129a5d66e
Mike Ziccardi <mikeziccardi@gmail.com>
2016-08-30 15:57:51 -0600

    adding table to keep organized list

--- jQuery.js
+++ jQuery.js
@@ -1,11 +1,16 @@
 var inputText = $('.inputText');
 var inputURL = $('.inputURL');
 var submitButton = $('.submitButton');
+var table = $('.table');
+var title = $('.title');
+var url = $('.url');
+
 
 submitButton.on('click', function(){
-  $('.title').append(inputText.val()+ '');  $('.title').append(inputURL.val());
-
-
-  $('.title').append('</br>');
+  $('.table').after('<tr>'+inputText+'</tr><tr>'+inputURL+'</tr>');
+  // $('.title').append(inputText.val()+ '');  $('.title').append(inputURL.val());
+  //
+  //
+  // $('.title').append('</br>');
 
 });
