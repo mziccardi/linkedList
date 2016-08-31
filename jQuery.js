bd887ee935f994ccfac8521594f62fca0eab1844
Mike Ziccardi <mikeziccardi@gmail.com>
2016-08-31 10:39:24 -0600

    MADE CHECKBOX AND REMOVE FUNCTIONAL

--- jQuery.js
+++ jQuery.js
@@ -9,6 +9,14 @@ var url = $('.url');
 submitButton.on('click', function(){
   var inputText = $('.inputText').val();
   var inputURL = $('.inputURL').val();
-  $('.table tr:last').after('<tr><td>'+inputText+'</td><td>'+inputURL+'</td><td class="checkbox"><input type="checkbox"></td><td><button class="remove">Remove</button></td></tr>');
+  $('.table tr:last').after('<tr class ="mike"><td>'+inputText+'</td><td>'+inputURL+'</td><td class="checkbox"><input type="checkbox"></td><td><button class="remove">Remove</button></td></tr>');
 
 });
+
+$('.table').on('click', '.checkbox', function(){
+    $(this).addClass('read');
+  });
+
+$('.table').on('click', '.remove', function(){
+    $(this).parents('.mike').remove();
+  });
