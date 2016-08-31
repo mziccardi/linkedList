8e7fa26bb5d782b8a748e9a715446010cd8d22af
Andrew Crist <andrew.l.crist@gmail.com>
2016-08-30 18:31:55 -0600

    Added button and checkbox functionality to row
    Still need to rework so that checkbox adds class and remove button removes row

--- jQuery.js
+++ jQuery.js
@@ -4,11 +4,29 @@ var submitButton = $('.submitButton');
 var table = $('.table');
 var title = $('.title');
 var url = $('.url');
-
+var removeButton = $('.removeButton');
+var bookmarkRow = $('.bookmarkRow');
 
 submitButton.on('click', function(){
   var inputText = $('.inputText').val();
   var inputURL = $('.inputURL').val();
-  $('.table tr:last').after('<tr><td>'+inputText+'</td><td>'+inputURL+'</td><td class="checkbox"><input type="checkbox"></td><td><button class="remove">Remove</button></td></tr>');
-
+  $('.table tr:last').after('<tr class="bookmarkRow"><td>'+inputText+'</td><td>'+inputURL+'</td><td class="checkbox"><input type="checkbox"></td><td><button class="removeButton">Remove</button></td></tr>');
 });
+
+// function addClass() {
+//   var checkbox = $('.checkbox');
+//   if (checkbox.checked) {
+//     $('input:checked').addClass('read');
+//   }
+// }
+
+$('input:checkbox').change(function(){
+   if($(this).is(":checked")) {
+       $('.bookmarkRow').addClass("read");
+   }
+});
+
+// removeButton.on('click', function() {
+//   var removeButton = $('.removeButton');
+//   $('.removeButton').closest('tr').remove();
+// });
